b3bbcf05dab3fd48522497ef68d6e8f7f1cbd6c2
mrthefakeperson <mrthefakeperson@gmail.com>
2017-10-15 12:52:16 -0700

    framework v2

--- framework.fs
+++ framework.fs
@@ -2,57 +2,117 @@ module Framework
 open System
 open System.Text.RegularExpressions
 
+// syntax:
+// let rec nameOfRule() = () |> buildRule()       <<-- the `() |> ` is for lazy eval. manipulation
+// and otherRule = () |> buildRule(nameOfRule)    <<-- don't pass `()` to nameOfRule
+
 type AST = T of string * AST list
 
 type rule = string list -> (AST * string list) option  // could be generic
 
-let addRecursionGuard (rule: rule) : rule =
-  let prevArg = ref None
-  fun tokens ->
-    try
-      if Some tokens = !prevArg
-       then None
-       else rule tokens
-    finally prevArg := Some tokens
+type Rule = unit -> rule
 
-type Rule(name) =
-  new() = Rule("")
-  // create matching function from <type>
-  member it.isOneOf([<ParamArray>]tokens: string[]) : rule = function
-    |x::rest when Array.exists ((=) x) tokens -> Some(T(x, []), rest)
-    |_ -> None
-  member it.is(token: string) : rule = it.isOneOf token
-  member it.matches(xpr: string) : rule = function
-    |x::rest when Regex.IsMatch(x, xpr) -> Some(T(x, []), rest)
-    |_ -> None
-  member it.isSequenceOf([<ParamArray>]rules: Lazy<rule>[]) : rule = fun tokens ->
-    // if not (Array.isEmpty rules) then  // be careful doing this! adding it to multiple rules in `isOneOf` will cause collisions
-    //   let x = rules.[0]
-    //   rules.[0] <- lazy addRecursionGuard (x.Force())
-    Array.fold (fun state (rule: Lazy<rule>) ->
-      let (|Rule|_|) = rule.Force()
-      match state with
-      |Some(acc, remainingTokens) ->
-        match remainingTokens with
-        |Rule(parsed, rest) -> Some(parsed::acc, rest)
-        |_ -> None
-      |None -> None
-     ) (Some([], tokens)) rules
-     |> function Some(acc, rest) -> Some(T(name, List.rev acc), rest) | _ -> None
-  member it.isOneOf([<ParamArray>]rules: Lazy<rule>[]) : rule = fun tokens ->
-    Array.tryPick ((|>) tokens) (Array.map (fun (e: Lazy<rule>) -> e.Force()) rules)
-  member it.isManyOf(rule: Lazy<rule>) : rule =
-    let (|Rule|_|) = rule.Force()
-    let rec matchWhile acc = function
-      |Rule(x, rest) -> matchWhile (T(name, [acc; x])) rest
-      |tokens -> acc, tokens
-    function Rule(x, rest) -> Some(matchWhile x rest) | _ -> None
+let Rename name (rule: Rule) () : rule =
+  let (|Rule|_|) = rule()
+  function Rule(T(_, ch), rest) -> Some(T(name, ch), rest) | _ -> None
+let Clean (rule: Rule) () : rule =
+  let (|Rule|_|) = rule()
+  let rec clean = function
+    T(name, ch) -> T(name, List.map clean ch |> List.collect (function T("", ch') -> ch' | e -> [e]))
+  function Rule(ast, rest) -> Some(clean ast, rest) | _ -> None
+let Equals(token: string) () : rule =
+  function x::rest when x = token -> Some(T(x, []), rest) | _ -> None
+let (!) = Equals
+let Matches(xpr: string) () : rule =
+  function x::rest when Regex.IsMatch(x, xpr) -> Some(T(x, []), rest) | _ -> None
+let (!!) = Matches
+let (/+) (x: Rule) (y: Rule) () : rule = fun tokens ->
+  let (|MatchesX|_|), (|MatchesY|_|) = x(), y()
+  match tokens with
+  |MatchesX(xAST, MatchesY(yAST, rest)) -> Some(T("", [xAST; yAST]), rest)
+  |_ -> None
+let IsSequenceOf([<ParamArray>]rules: Rule[]) : rule = Array.reduce (/+) rules ()
+let (/|) (x: Rule) (y: Rule) () : rule = fun tokens ->
+  let (|MatchesX|_|), (|MatchesY|_|) = x(), y()
+  match tokens with
+  |MatchesX(ast, rest) | MatchesY(ast, rest) -> Some(ast, rest)
+  |_ -> None
+let IsOneOf([<ParamArray>]rules: Rule[]) : rule = Array.reduce (/|) rules ()
+let IsManyOf(rule: Rule) () : rule =
+  let (|Rule|_|) = rule()
+  let rec matchWhile acc = function
+    |Rule(ast, rest) -> matchWhile (ast::acc) rest
+    |tokens -> T("", List.rev acc), tokens
+  function Rule(ast, rest) -> Some(matchWhile [ast] rest) | _ -> None
+let (~+) = IsManyOf
+let IsOptional(rule: Rule) () : rule =
+  let (|Rule|_|) = rule()
+  function Rule(ast, rest) -> Some(ast, rest) | tokens -> Some(T("", []), tokens)
+let (~~) = IsOptional
 
-let optional ((|Rule|_|): rule) = function Rule x -> Some x | tokens -> Some(T("", []), tokens)
+// type RuleDetails = {name: string; clean: bool}
 
-let debug msg (rule: rule) e =
-  printfn "%s" msg
-  printfn "%A" e
-  let yld = rule e
-  printfn "result: %A" yld
-  yld
+// type RuleBuilder(detailList') =
+//   let mutable detailList = detailList'
+//   member x.Bind((value: Rule), xpr) =
+//     match detailList with
+//     |{name=name; clean=clean}::tl ->
+//       detailList <- tl
+//       xpr value |> Rename name |> (if clean then Clean else id)
+//     |[] -> failwith "found an undefined rule"
+
+// RuleBuilder([]) {
+//   let! x = "x", Rule.Is "x"
+//   ()
+// }
+
+// let addRecursionGuard (rule: rule) : rule =
+//   let prevArg = ref None
+//   fun tokens ->
+//     try
+//       if Some tokens = !prevArg
+//        then None
+//        else rule tokens
+//     finally prevArg := Some tokens
+
+// type Rule(name) =
+//   new() = Rule("")
+//   // create matching function from <type>
+//   member it.isOneOf([<ParamArray>]tokens: string[]) : rule = function
+//     |x::rest when Array.exists ((=) x) tokens -> Some(T(x, []), rest)
+//     |_ -> None
+//   member it.is(token: string) : rule = it.isOneOf token
+//   member it.matches(xpr: string) : rule = function
+//     |x::rest when Regex.IsMatch(x, xpr) -> Some(T(x, []), rest)
+//     |_ -> None
+//   member it.isSequenceOf([<ParamArray>]rules: Lazy<rule>[]) : rule = fun tokens ->
+//     // if not (Array.isEmpty rules) then  // be careful doing this! adding it to multiple rules in `isOneOf` will cause collisions
+//     //   let x = rules.[0]
+//     //   rules.[0] <- lazy addRecursionGuard (x.Force())
+//     Array.fold (fun state (rule: Lazy<rule>) ->
+//       let (|Rule|_|) = rule.Force()
+//       match state with
+//       |Some(acc, remainingTokens) ->
+//         match remainingTokens with
+//         |Rule(parsed, rest) -> Some(parsed::acc, rest)
+//         |_ -> None
+//       |None -> None
+//      ) (Some([], tokens)) rules
+//      |> function Some(acc, rest) -> Some(T(name, List.rev acc), rest) | _ -> None
+//   member it.isOneOf([<ParamArray>]rules: Lazy<rule>[]) : rule = fun tokens ->
+//     Array.tryPick ((|>) tokens) (Array.map (fun (e: Lazy<rule>) -> e.Force()) rules)
+//   member it.isManyOf(rule: Lazy<rule>) : rule =
+//     let (|Rule|_|) = rule.Force()
+//     let rec matchWhile acc = function
+//       |Rule(x, rest) -> matchWhile (T(name, [acc; x])) rest
+//       |tokens -> acc, tokens
+//     function Rule(x, rest) -> Some(matchWhile x rest) | _ -> None
+
+// let optional ((|Rule|_|): rule) = function Rule x -> Some x | tokens -> Some(T("", []), tokens)
+
+// let debug msg (rule: rule) e =
+//   printfn "%s" msg
+//   printfn "%A" e
+//   let yld = rule e
+//   printfn "result: %A" yld
+//   yld
--- test.fsx
+++ test.fsx
@@ -11,28 +11,29 @@ a: {
 
 let tokens = Lexer.tokenize text |> List.ofSeq
 
-let _var = lazy Rule().matches("[a-z A-Z][a-z 0-9 A-Z _]*")
-let _string = lazy Rule().matches("\"(\\\"|[^\"])*\"")
-let _int = lazy Rule().matches("-?[0-9]+")
-let rec value =
-  lazy Rule().isOneOf(
-    lazy Rule("object").isSequenceOf(lazy Rule().is "{", lazy optional (assignmentSeq.Force()), lazy Rule().is "}"),
-    lazy Rule("list").isSequenceOf(lazy Rule().is "[", lazy optional (valueSeq.Force()), lazy Rule().is "]"),
-    lazy Rule().isOneOf(_string, _int)
-   )
-and valueSeq: Lazy<rule> =
-  lazy Rule().isSequenceOf(value, lazy optional (Rule().isManyOf(lazy Rule().isSequenceOf(lazy Rule().is ",", value))))
-and assignment = lazy Rule("assignment").isSequenceOf(_var, lazy Rule().is ":", value)
-and assignmentSeq: Lazy<rule> =
-  lazy Rule().isSequenceOf(assignment, lazy optional (Rule().isManyOf(lazy Rule().isSequenceOf(lazy Rule().is ",", assignment))))
+let _var = !!"[a-z A-Z][a-z 0-9 A-Z _]*"
+let _string = !!"\"(\\\"|[^\"])*\""
+let _int = !!"-?[0-9]+"
+let rec value() = () |> (
+  Rename "object" (Equals "{" /+ ~~assignmentSeq /+ !"}")
+   /| Rename "list" (Equals "[" /+ ~~valueSeq /+ !"]")
+   /| _string /| _int
+ )
+and valueSeq() = () |> value /+ ~~(+(!"," /+ value))
+and assignment() = () |> Rename "assignment" (_var /+ !":" /+ value)
+and assignmentSeq() = () |> Clean (assignment /+ ~~(+(!"," /+ assignment)))
 
-// let anything: Lazy<rule> = lazy function hd::rest -> Some(T(hd, []), rest) | _ -> None
-// let var = lazy Rule().is("[xyz]")
-// let prop = lazy Rule("prop").isSequenceOf(var, lazy Rule().is "=", anything)
-// let openTag =  // note: only the last one of `isSequenceOf` should be `isOneOf`, or otherwise it won't work (fail on one branch -> doesn't check others)
-//   lazy Rule("open tag").isSequenceOf(lazy Rule().is "<", var, lazy optional (Rule().isManyOf prop), lazy Rule().is ">")
-// let closeTag = lazy Rule("close tag").isSequenceOf(lazy Rule().is "<", lazy Rule().is "/", var, lazy Rule().is ">")
-// let containedTag = lazy Rule("open tag").isSequenceOf(lazy Rule().is "<", var, lazy optional (Rule().isManyOf prop), lazy Rule().is "/", lazy Rule().is ">")
-// let rec value = lazy Rule().isOneOf(containedTag, lazy Rule().isSequenceOf(openTag, lazy optional (Rule().isManyOf value), closeTag))
-
-// value.Force() tokens
+// let _var = lazy Rule().matches("[a-z A-Z][a-z 0-9 A-Z _]*")
+// let _string = lazy Rule().matches("\"(\\\"|[^\"])*\"")
+// let _int = lazy Rule().matches("-?[0-9]+")
+// let rec value =
+//   lazy Rule().isOneOf(
+//     lazy Rule("object").isSequenceOf(lazy Rule().is "{", lazy optional (assignmentSeq.Force()), lazy Rule().is "}"),
+//     lazy Rule("list").isSequenceOf(lazy Rule().is "[", lazy optional (valueSeq.Force()), lazy Rule().is "]"),
+//     lazy Rule().isOneOf(_string, _int)
+//    )
+// and valueSeq: Lazy<rule> =
+//   lazy Rule().isSequenceOf(value, lazy optional (Rule().isManyOf(lazy Rule().isSequenceOf(lazy Rule().is ",", value))))
+// and assignment = lazy Rule("assignment").isSequenceOf(_var, lazy Rule().is ":", value)
+// and assignmentSeq: Lazy<rule> =
+//   lazy Rule().isSequenceOf(assignment, lazy optional (Rule().isManyOf(lazy Rule().isSequenceOf(lazy Rule().is ",", assignment))))
