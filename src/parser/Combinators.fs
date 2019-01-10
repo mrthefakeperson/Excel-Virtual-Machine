a2c8e0d70acdd3d998c72482e868e181ad7d0c81
Charles Lei <mrthefakeperson@gmail.com>
2019-01-09 23:50:37 -0800

    library v3

--- src/parser/Combinators.fs
+++ src/parser/Combinators.fs
@@ -8,7 +8,9 @@ open Lexer.Token
 
 type ASTx = T of string * ASTx list
 
-type Result = Yes of ASTx * Token list | No of failMessage: string
+type Result =
+  |Yes of ASTx * Token list
+  |No of string * Token list  // fail message associates with longest successful parse path
 
 type rule = Token list -> Result
 
@@ -16,7 +18,7 @@ type Rule = unit -> rule
 let transformASTx f (rule: Rule) e =
   match rule() e with
   |Yes(ast, rest) -> Yes(f ast, rest)
-  |No err -> No err
+  |no -> no
 
 let Rename name (rule: Rule) () : rule =
   transformASTx (fun (T(_, ch)) -> T(name, ch)) rule
@@ -31,20 +33,28 @@ let (!!!) = Clean
 
 let Pass = Rename ""
 let Equal(token: string) () : rule = function
-  |x::rest when x.value = token -> Yes(T(x.value, []), rest)
-  |tokens -> No(sprintf "expected %s before %A" token (List.map (fun e -> e.value) tokens))
+  |{value=v}::rest when v = token -> Yes(T(v, []), rest)
+  |tokens ->
+    let token_values = List.map (fun e -> e.value) tokens
+    let error = sprintf "expected %s before %A" token token_values
+    No(error, tokens)
 let (!) = Equal >> Pass
 let (~%) = Equal
 
 let Match(xpr: string) () : rule = function
-  |x::rest when Regex.Match(x.value, xpr).Value = x.value -> Yes(T(x.value, []), rest)
-  |tokens -> No(sprintf "expected (%s) before %A" xpr (List.map (fun e -> e.value) tokens))
+  |{value=v}::rest when Regex.Match(v, xpr).Value = v -> Yes(T(v, []), rest)
+  |tokens ->
+    let token_values = List.map (fun e -> e.value) tokens
+    let error = sprintf "expected (%s) before %A" xpr token_values
+    No(error, tokens)
 let (!!) = Match >> Pass
 let (~%%) = Match
 
 let EOF () : rule = function
   |[] -> Yes(T("", []), [])
-  |tokens -> No(sprintf "expected EOF at %A" (List.map (fun e -> e.value) tokens))
+  |tokens ->
+    let token_values = List.map (fun e -> e.value) tokens
+    No(sprintf "expected EOF at %A" token_values, tokens)
 
 let (+/) (x: Rule) (y: Rule) () : rule = fun tokens ->
   match x() tokens with
@@ -54,12 +64,23 @@ let SequenceOf(rules: #seq<Rule>) : Rule = Seq.reduce (+/) rules
 
 let (|/) (x: Rule) (y: Rule) () : rule = fun tokens ->
   match x() tokens with
-  |No err1 ->
+  |No(err1, r1) ->
     match y() tokens with
-    |No err2 -> No(err1 + "\n" + err2)
+    |No(err2, r2) when r2.Length < r1.Length -> No(err2, r2)
+    |No _ -> No(err1, r1)
     |yes -> yes
   |yes -> yes
 let OneOf(rules: #seq<Rule>) : Rule = Seq.reduce (|/) rules
+//let OneOf(rules: #seq<Rule>) () : rule = fun tokens ->
+//  Seq.fold (fun acc x ->
+//    match acc with
+//    |No(err, r) ->
+//      match x() tokens with
+//      |No(err2, r2) when r2.Length < r.Length -> No(err2, r2)
+//      |No _ -> No(err, r)
+//      |yes -> yes
+//    |yes -> yes
+//   ) (No("placeholder", tokens)) rules
 
 let Optional(rule: Rule) () : rule =
   fun tokens -> match rule() tokens with No _ -> Yes(T("", []), tokens) | yes -> yes
