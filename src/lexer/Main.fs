7f78ab4fbd0fcba37a5eaaf93ddb71475de59abb
Charles Lei <mrthefakeperson@gmail.com>
2019-05-20 02:33:03 -0400

    update lexer

--- src/lexer/Main.fs
+++ src/lexer/Main.fs
@@ -1,33 +1,96 @@
 ﻿module Lexer.Main
+open System
+open Utils
 open RegexUtils
+open ParserCombinators
+open ParserCombinators.Atoms.String
+open CompilerDatatypes.DT
+open CompilerDatatypes.Token
 open Lexer.Preprocessor
 
-type Token = {
-  value: string
-  line: int
-  column: int
-  file: SourceFile
+let comment = %%COMMENT_LINE |/ %%COMMENT_BLOCK
+let garbage =
+  %%WHITESPACE +/ comment +/ %%WHITESPACE ->/ fun ((a, b), c) -> a.Length + b.Length + c.Length
+let lexeme tkn (rule: Rule<string, Value>) : Rule<string, Token> = fun input ->
+  match (garbage +/ rule) input with
+  |Ok(rest, (spaces, res)) -> Ok(rest, {tkn with value = res; col = -input.Length + spaces})
+  |Error msg -> Error msg
+
+let token_num = SequenceOf {
+  let! radix =
+    OneOf [
+      !"0x" ->/ fun _ -> 16
+      !"0" ->/ fun _ -> 8
+      !"" ->/ fun _ -> 10
+     ]
+  let! integer = %%"[0-9]+"
+  let! has_dot = Optional !"." ->/ Option.isSome
+  let! decimal = %%"[0-9]*"
+  let! type_suffix = %%"[A-Za-z]*"
+  return
+    match (radix, has_dot, type_suffix.ToLower()) with
+    |(_, false, "") -> Lit(string (Convert.ToInt32(integer, radix)), DT.Int)
+    |(_, false, ("l" | "ll")) -> Lit(string (Convert.ToInt64(integer, radix)), DT.Int64)
+    |(10, true, "f") -> Lit(string (Convert.ToSingle(integer + "." + decimal)), DT.Float)
+    |(10, true, ("" | "lf" | "llf")) ->
+      Lit(string (Convert.ToDouble(integer + "." + decimal)), DT.Double)
+    |_ -> failwith "unknown integer literal"
  }
-   
-// TODO: test
-let rec tokenize_line: int -> LexerLine -> Token list = fun col line ->
-  match line.value with
-  |Regex' NUM_FLOAT (hd, tl)
-  |Regex' "[\w]+" (hd, tl)
-  |Regex' STRING (hd, tl)
-  |Regex' CHAR (hd, tl)
-  |Regex' "(--|\+\+|>=|<=|==|!=|&&|\|\||&|\+=|-=|\*=|/=|&=|\|=|->)" (hd, tl)  // other tokens
-  |Regex' "[{}();,=+\-*/%<>\[\].:]" (hd, tl)
-  |Regex' WHITESPACE (hd, tl) ->
-    {value = hd.Trim(); line = line.line; column = col + hd.Length - hd.TrimStart().Length; file = line.file}
-     :: tokenize_line (col + hd.Length) {line with value = tl}
-  |"" -> []
-  |unexpected -> failwithf "Lexer: unexpected %s" unexpected
 
-let tokenize: LexerLine list -> Token list =
-  List.collect (tokenize_line 0) >> List.filter (function {value = ""} -> false | _ -> true)
+let token_var = %%"[A-Za-z_]\w*" ->/ fun name -> Var(name, DT.Void)
 
-let tokenize_text (txt: string) =
-  txt
-   |> preprocessor_pass "source"
-   |> tokenize
+let escape = SequenceOf {
+  do! !"\\"
+  let! c = %%"."
+  return
+    match c with
+    |"n" -> '\n' | "t" -> '\t' | "r" -> '\r' | "f" -> '\f' | "\\" -> '\\' | "0" -> '\000'
+    |_ -> failwithf "unknown escape character: \\%s" c
+ }
+
+let token_string = SequenceOf {
+  do! !"\""
+  let! chars = OptionalListOf (escape ->/ string |/ %%"[^\"\\\\]")
+  do! !"\""
+  return Lit("\"" + String.concat "" chars + "\"", DT.Ptr DT.Byte)
+ }
+
+let token_char = SequenceOf {
+  do! !"'"
+  let! c = escape
+  do! !"'"
+  return Lit(string (byte c), DT.Byte)
+ }
+
+let token_symbol =
+  (%%"(--|\+\+|>=|<=|==|!=|&&|\|\||&|\+=|-=|\*=|/=|&=|\|=|->)" |/ %%"[{}();,=+\-*/%<>\[\].:]")
+   ->/ fun name -> Lit(name, DT.Void)
+
+let preprocess = %%PREPROCESSOR_LINE ->/ fun name -> Lit(name, DT.Void)
+
+let parse_tokens tkn : Rule<String, Token list> =
+  let parse_token =
+    OneOf [token_num; token_var; token_string; token_char; token_symbol; preprocess]
+  OptionalListOf (lexeme tkn parse_token) +/ (garbage +/ End) ->/ fst
+
+let rec tokenize filename (s: string) : Token list =
+  let rec get_rc i =
+    if i = 0 then (0, 0)
+    elif s.[i - 1] = '\n' then let (line, _) = array_rc.[i - 1] in (line + 1, 0)
+    else let (line, col) = array_rc.[i - 1] in (line, col + 1)
+  and array_rc: (int * int)[] = Array.init s.Length get_rc
+  run_parser (parse_tokens {deftkn with file = filename}) s
+   |> List.map (fun tkn ->
+        let pos = s.Length + tkn.col
+        let (line, col) = array_rc.[pos]
+        {tkn with line = line; col = col}
+       )
+   |> List.collect (function
+        |{value = Lit(Directive("#include", incl), _)} ->  // handle this as a special case for recursion
+          let (file, src) = get_include incl
+          tokenize file src
+        |tkn -> [tkn]
+       )
+   |> preprocessor_pass
+
+let tokenize_text = tokenize (Builtin "source")
--- src/lexer/Preprocessor.fs
+++ src/lexer/Preprocessor.fs
@@ -1,94 +1,45 @@
 ﻿module Lexer.Preprocessor
 open System.IO
-open System.Text.RegularExpressions
+open Utils
 open RegexUtils
-
-// |Regex' pattern (matched_prefix, rest)
-let (|Regex'|_|) pattern (s: string) =
-  Option.map (fun (m: Match) -> (m.Value, s.Substring(m.Value.Length)))
-   ((|Regex|_|) ("^" + WHITESPACE + pattern) s)
-
-// TODO: test
-let rec tokenize_strip_comments: string -> string list = fun txt ->
-  match txt with
-  |Regex' COMMENT_LINE (cmt, tl) | Regex' COMMENT_BLOCK (cmt, tl) ->
-    Regex.Replace(cmt, "[^\n]", "")::tokenize_strip_comments tl
-  |Regex' PREPROCESSOR_LINE (hd, tl)
-  |Regex' NUM_FLOAT (hd, tl)
-  |Regex' "[\w]+" (hd, tl)
-  |Regex' STRING (hd, tl)
-  |Regex' CHAR (hd, tl)
-  |Regex' "(--|\+\+|>=|<=|==|!=|&&|\|\||&|\+=|-=|\*=|/=|&=|\|=|->)" (hd, tl)  // other tokens
-  |Regex' "[{}();,=+\-*/%<>\[\].:]" (hd, tl)
-  |Regex' WHITESPACE (hd, tl) -> hd::tokenize_strip_comments tl
-  |"" -> []
-  |unexpected -> failwithf "Lexer: unexpected %s" unexpected
-
-// remove comments, but disregard the contents of strings
-let rec strip_comments: string -> string = tokenize_strip_comments >> String.concat ""
-
-type SourceFile = Builtin of string | Local of string
-
-type LexerLine = {
-  value: string
-  file: SourceFile
-  line: int
- }
-
-let lines: SourceFile -> string -> LexerLine list = fun src_file src ->
-  src.Split '\n' |> List.ofArray
-   |> List.mapi (fun i (e: string) -> {value = e.TrimEnd(); file = src_file; line = i + 1})
-   |> List.rev
-   |> List.fold (fun acc line ->
-        match acc with
-        |[] -> [line]
-        |hd::tl when line.value.EndsWith(@"\") -> {line with value = line.value + hd.value}::tl
-        |_ -> line::acc
-       ) []
-   |> List.map (fun e -> {e with value = e.value.Replace("\\\n", "")})
+open CompilerDatatypes.Token
 
 let (|Directive|_|) (content: string) =
   match content.TrimStart() with
   |Regex PREPROCESSOR_LINE _ ->
-    let [|""; content'|] | Strict content' = content.Split([|'#'|], 2)
-    let [|dir; content''|] | Strict(dir, content'') = content'.Split([|' '|], 2) 
-    Some(dir, content''.Trim())
+    let [|dir; content'|] | Strict(dir, content') = content.Split([|' '|], 2) 
+    Some(dir, content'.Trim().Split ' ')
   |_ -> None
 
-type PreprocessState = {
-  macros: Map<string, string Option>
- }
-
-let builtin_files: Map<string, LexerLine list> =
-  Map <| List.map (fun (name, src) -> (name, lines (Builtin name) src)) [
+let builtin_files: Map<string, string> =
+  Map [
     "stdio.h", ""
     "limits.h", ""
     "string.h", ""
    ]
 
-let rec preprocessor: LexerLine list -> LexerLine list = function
-  |[] -> []
-  |{value = Directive("include", content)}::tl ->
-    match content with
-    |Regex "^<(.*)>$" m ->
-      let filename = m.Groups.[1].Value
-      let file =
-        Map.tryFind filename builtin_files
-         |> Option.defaultWith (fun () -> failwithf "could not include <%s>" filename)
-      file @ preprocessor tl
-    |Regex "^\"(.*)\"$" m ->
-      let filename = m.Groups.[1].Value
-      let file =
-        try File.ReadAllText filename
-        with _ -> failwithf "could not include \"%s\"" filename
-         |> lines (Local filename)
-      file @ preprocessor tl
-    |_ -> failwith "bad include format"
-  |{value = Directive("define", content)}::tl ->
-    match content.Split(' ') with
-    |_ -> failwith "macros not supported"
-  |{value = Directive _}::_ -> failwith "unknown directive"
-  |hd::tl -> hd::preprocessor tl
-  
-let rec preprocessor_pass: string -> string -> LexerLine list = fun src_file src ->
-  lines (Local src_file) (strip_comments src) |> preprocessor
+let get_include: string[] -> SourceFile * string = function
+  |[|Regex "^<(.*)>$" mtch|] ->
+    let filename = mtch.Captures.[0].Value
+    match Map.tryFind filename builtin_files with
+    |Some src -> (Builtin filename, src)
+    |None -> failwithf "bad include: could not find <%s>" filename
+  |[|Regex "^\"(.*)\"$" mtch|] ->
+    let filename = mtch.Captures.[0].Value
+    try (Local filename, File.ReadAllText filename)
+    with :? FileNotFoundException -> failwithf "bad include: could not find %A" filename
+  |_ -> failwith "bad include"
+
+type PreprocessState = {
+  macros: Map<string, string Option>
+ }
+
+let preprocessor_pass: Token list -> Token list =
+  let rec preprocess state = function
+    |[] -> []
+    |{value = Lit(Directive("#define", content), _)}::_ ->
+      match content with
+      |_ -> failwith "macros not supported"
+    |{value = Lit(Directive(dir, _), _)}::_ -> failwithf "directive not supported: %s" dir
+    |hd::tl -> hd::preprocess state tl
+  preprocess {macros = Map.empty}
